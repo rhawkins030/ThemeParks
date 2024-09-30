7783907b0c763b838a0cdafc3ffefc2daf35a212
Ryan Hawkins <rhawkins030@gmail.com>
2024-09-30 19:43:27 -0400

    cloudflare

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,7 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
+  preset: "cloudflare_pages",
   routeRules: {
     '/github/**': { redirect: 'https://github.com/rhawkins030/ThemeParks' },
     '/': { redirect: '/api/' }
